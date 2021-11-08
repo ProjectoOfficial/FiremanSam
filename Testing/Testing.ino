e24ee59466a3cda85c5f439e3da9b2bf44ee0296
ProjectoOfficial <miniprojectsofficial@gmail.com>
2021-11-08 22:50:23 +0100

    Firebase

---
+++ Testing/Testing.ino
@@ -0,0 +1,129 @@
+#include "DHT.h"
+#include "Adafruit_CCS811.h"
+#include <CCS811.h>
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+#define DHTPIN 23
+#define DHTTYPE DHT11
+#define LED 5
+
+
+FirebaseData fData;
+FirebaseJson json;
+
+DHT dht(DHTPIN, DHTTYPE);
+CCS811 sensor;
+
+float weights[] = { -0.004163, 0.005974, 005523, 0.005606};
+float bias = -0.007646;
+float means[] = {54.023747, 25.116755, 1655.704485, 1035.536939};
+float devs[] = {12.707672, 4.145223, 1496.357791, 2692.752151};
+
+size_t updateTime = 0;
+
+/**********************VOID SETUP********************************/
+void setup() {
+  Serial.begin(115200);
+
+  while (sensor.begin() != 0) {
+    Serial.println("failed to init chip, please check if the chip connection is fine");
+    delay(1000);
+  }
+
+  dht.begin();
+  sensor.setMeasCycle(sensor.eCycle_250ms);
+  pinMode(LED, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  size_t start_time = millis();
+  while (WiFi.status() != WL_CONNECTED)
+    if (millis() - start_time > 500) {
+      Serial.print(".");
+      start_time = millis();
+    }
+
+  Serial.println("Connessione");
+  Serial.println();
+  Serial.print("WiFi Connesso, indirizzo IP:");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Firebase.setReadTimeout(fData, 1000 * 60);
+  Firebase.setwriteSizeLimit(fData, "tiny");
+
+  Serial.println();
+  Serial.println("-------------------------------------");
+  Serial.println("Fatto! WiFi connesso!");
+  updateTime = millis();
+}
+
+/**********************DHT11 READ********************************/
+void humidity_and_temperature(float *h, float *t) {
+  *h = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  if (isnan(*h) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+  }
+
+  *t = dht.computeHeatIndex(temp, *h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(*h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(*t);
+  Serial.print(F("°C "));
+}
+
+/**********************CCS811 READ********************************/
+void co2_and_tvoc(int *co2, int *tvoc) {
+  if (sensor.checkDataReady() == true) {
+    *co2 = sensor.getCO2PPM();
+    *tvoc = sensor.getTVOCPPB();
+    Serial.print("CO2: ");
+    Serial.print(*co2);
+    Serial.print("ppm, TVOC: ");
+    Serial.print(*tvoc);
+    Serial.println("ppb");
+
+  } else {
+    Serial.println("Data is not ready!");
+  }
+  sensor.writeBaseLine(0x847B);
+}
+
+/**********************VOID LOOP********************************/
+void loop() {
+  if (millis() - updateTime > 500) {
+    float humidity, temperature ;
+    int co2, tvoc;
+    humidity_and_temperature(&humidity, &temperature);
+    co2_and_tvoc(&co2, &tvoc);
+
+    float datas[] = {humidity, temperature, co2, tvoc};
+    float score = 0;
+    for (int i = 0; i < 4; i++) {
+      score += (datas[i] - means[i]) / devs[i] * weights[i];
+    }
+    score += bias;
+    Serial.print("Score: ");
+    Serial.print(score);
+
+    if (score > 0) {
+      Serial.println("\nFire detected!");
+      digitalWrite(LED, HIGH);
+    }
+    else {
+      Serial.println("\nNothing");
+      digitalWrite(LED, LOW);
+    }
+    Serial.print("\n");
+    
+    json.set("/value", score);
+    Firebase.updateNode(fData, "/score", json);
+    updateTime = millis();
+  }
+}
